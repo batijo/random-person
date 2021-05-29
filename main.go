39568b03ef964c21da36bec04aa3899906e242fd
batijo <titasbajorunas@gmail.com>
2021-05-29 08:20:32 +0300

    TLS setup will be on nginx

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"os"
-	"strconv"
 
 	"github.com/batijo/random-person/database"
 	"github.com/batijo/random-person/server"
@@ -34,17 +33,5 @@ func main() {
 	srv := server.New(&db)
 	db.InsertData(configFolder, namesFile, surnamesFile)
 	log.Println("Server is running on: ", os.Getenv("RP_IP")+":"+os.Getenv("RP_PORT"))
-	prod, err := strconv.ParseBool(os.Getenv("RP_PROD"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	if prod {
-		log.Fatal(srv.ListenTLS(
-			":"+os.Getenv("RP_PORT"),
-			configFolder+os.Getenv("RP_CERT_FILE"),
-			configFolder+os.Getenv("RP_KEY_FILE"),
-		))
-	} else {
-		log.Fatal(srv.App.Listen(":" + os.Getenv("RP_PORT")))
-	}
+	log.Fatal(srv.App.Listen(":" + os.Getenv("RP_PORT")))
 }
